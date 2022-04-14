ed4da6bedeec495c9e8a47a783e3a34542bb16ce
diep tam <minhtam.diep@citynow.vn>
2022-04-14 14:44:43 +0700

    [documents] update readme file and requirements file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+
 # model_hub
+
 This is where i save interested model
+
+1. Setup environments:
+
+```powershell
+conda create --name model_env python=3.6
+conda activate model_env
+pip install torch==1.8.0+cpu torchvision==0.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install -r requirements.txt
+```
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 MODEL_BACKBONE = 'IR_GHOST_MODEL'
 INPUT_SIZE = [112, 112]
 
+# === CONVERT ONNX CONFIG ===
+# MODEL SUPPORT
+# ir-50, ir-101, ir-152, irse-50, irse-101, irse-152, ghostnet, ir_ghost_50,
+# ir_ghost_101, ir_ghost_152, irse_ghost_50, irse_ghost_101, irse_ghost_152
+MODEL = ''
+
 # ==== TEST CONFIG ===
 WEIGHT_PATH = 'weight/'
 NUM_SAMPLES = 100
