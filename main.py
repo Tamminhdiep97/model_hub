3db49677a3211bdbadad77c3aaf11f13b80a1a68
tamminhdiep97 <tamdiepminh97@gmail.com>
2022-04-23 13:00:21 +0700

    [reformat] reformat code

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ if __name__ == '__main__':
     if conf.MODEL.upper() == 'ALL':
         for key in MODEL.keys():
             print('exporting: {}'.format(key))
-            utils.export_onnx(MODEL[key], os.path.join(conf.WEIGHT_PATH, key + '.onnx'))
+            utils.export_onnx(
+                MODEL[key], os.path.join(conf.WEIGHT_PATH, key + '.onnx')
+            )
     elif MODEL is not None:
         print('exporting: {}'.format(key))
-        utils.export_onnx(MODEL, os.path.join(conf.WEIGHT_PATH, conf.MODEL.upper()))
+        utils.export_onnx(
+            MODEL, os.path.join(conf.WEIGHT_PATH, conf.MODEL.upper())
+        )
