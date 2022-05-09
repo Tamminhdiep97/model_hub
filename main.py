360ba312cefb3b3260cb24c021b4e609e643e711
tamminhdiep97 <tamdiepminh97@gmail.com>
2022-05-09 14:55:06 +0700

    [bug] fix bug in saving single onnx model in main, add GHOSTNET into config

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ if __name__ == '__main__':
     if conf.MODEL.upper() == 'ALL':
         for key in MODEL.keys():
             print('exporting: {}'.format(key))
-            utils.export_onnx(
-                MODEL[key], os.path.join(conf.WEIGHT_PATH, key + '.onnx')
-            )
+            utils.export_onnx(MODEL[key], os.path.join(conf.WEIGHT_PATH, key + '.onnx'))
     elif MODEL is not None:
-        print('exporting: {}'.format(key))
+        print('exporting: {}'.format(MODEL))
         utils.export_onnx(
-            MODEL, os.path.join(conf.WEIGHT_PATH, conf.MODEL.upper())
+            MODEL, os.path.join(conf.WEIGHT_PATH, conf.MODEL.upper() + '.onnx')
         )
